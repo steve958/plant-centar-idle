087ed97ef6934c3940dbc408ca3b25085ea7c398
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2024-08-23 19:00:21 +0200

    idle cover

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './assets/PlantCLogo.jpg'
+import slogo from './assets/logo.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <img src={logo} alt="" />
+      <img src={slogo} alt="" />
+      Internet stranica u pripremi. Vidimo se uskoro 😊
+    </div>
+  )
+}
+
+export default App
